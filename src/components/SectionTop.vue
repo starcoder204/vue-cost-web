f423e4a8f480dee8db7975731367bfc7e0e7f760
Lane Welling <lwelli77777@gmail.com>
2025-04-06 13:04:55 -0600

    bold and italic text in insurance section

--- src/components/SectionTop.vue
+++ src/components/SectionTop.vue
@@ -1,7 +1,8 @@
 <template>
     <section class="section">
+        
         <p><strong>Underinsurance Prevalence:</strong> Research indicates that a significant portion of
-            homeowners are underinsured, with estimates suggesting that 74% to 75% of homeowners might not have
+            homeowners are underinsured, <b><i>some estimates suggesting that up to 75%</i></b> of homeowners might not have
             adequate coverage to rebuild their homes after a disaster.</p>
         <br />
         <p>
