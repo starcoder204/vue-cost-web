365ac8dacf91d29a43df882adbe9530a8c9cb17f
Lane Welling <lwelli77777@gmail.com>
2025-04-06 13:11:48 -0600

    Show insurance section for spec builder

--- src/pages/SpecBuilder.vue
+++ src/pages/SpecBuilder.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="main-container">
-            <!-- <SectionTop /> -->
+            <SectionTop />
             
             <InputSection
                 :defaultCostPerSqftInput="700"
