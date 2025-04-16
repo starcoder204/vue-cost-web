071913b3c9b4de73725c34e097801d119f7166ac
unknown <starcoder204@gmail.com>
2025-04-15 21:38:40 -0400

    modal

--- src/pages/Referral.vue
+++ src/pages/Referral.vue
@@ -89,6 +89,64 @@
         <div v-if="isLoading" class="loading-overlay">
             Waiting...
         </div>
+
+        <!-- Modal -->
+        <div
+        v-if="showModal"
+        class="fixed inset-0 z-80 flex items-center justify-center bg-black bg-opacity-50"
+        role="dialog"
+        aria-labelledby="sign-out-modal-label"
+        tabindex="-1"
+        >
+            <div class="bg-white rounded-xl shadow-lg dark:bg-neutral-900 sm:max-w-xs w-full m-3 p-4 sm:p-10 relative">
+                <!-- Close Button -->
+                <button
+                @click="showModal = false"
+                type="button"
+                class="absolute top-2 right-2 size-8 inline-flex justify-center items-center rounded-full bg-gray-100 text-gray-800 hover:bg-gray-200 dark:bg-neutral-700 dark:hover:bg-neutral-600"
+                aria-label="Close"
+                >
+                    &times;
+                </button>
+
+                <!-- Modal Content -->
+                <div class="text-center">
+                <span class="mb-4 inline-flex justify-center items-center size-15.5 rounded-full border-4 border-yellow-50 bg-yellow-100 text-yellow-500 dark:bg-yellow-700 dark:border-yellow-600 dark:text-yellow-100">
+                    <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                    </svg>
+                </span>
+
+                <div class="flex justify-center py-5">
+                    <svg width="40" height="40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="12" cy="12" r="10" stroke="green" stroke-width="2" fill="none"/>
+                        <path d="M7 12l3 3 7-7" stroke="green" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </div>               
+
+
+                <h3 class="mb-2 text-2xl font-bold text-gray-800 dark:text-neutral-200">Referral Submitted Successfully</h3>
+                <p class="text-gray-500 dark:text-neutral-500 text-sm">
+                    Thanks for the connection — our team will reach out and keep you updated.
+                </p>
+
+                <div class="my-6 grid gap-y-2">
+                    <button
+                    type="button"
+                    class="py-2.5 px-4 w-full inline-flex justify-center items-center gap-x-2 text-md font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700"
+                    >
+                    Submit a New Referral
+                    </button>
+
+                    <button type="button" class="py-2.5 px-4 w-full inline-flex justify-center items-center gap-x-2 text-md font-medium rounded-lg border border-gray-200 bg-gray-200 text-gray-800 shadow-2xs hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-hidden focus:bg-gray-50 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
+                        Go to Referrals
+                    </button>
+                    
+                </div>
+                </div>
+            </div>
+        </div>
+
     </section>
 </template>
 
@@ -111,13 +169,17 @@ export default {
             }, ...serviceTypesArray],
             location: 840,
             locationOptions: countries,
+            showModal: false,
             isLoading: false,
         }
     },
     created() {
     },
+    mounted() {
+    },
     methods: {
         async hanldeSubmit() {
+            this.showModal = true
             if (!this.referralName || !this.projectName || !this.referralEmail || !this.serviceType || !this.location) return;
         }
     }
